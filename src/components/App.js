925670235a7c6247f928a0715f4c13e4af951752
HyunPark85 <parkhyun148@gmail.com>
2019-05-27 13:22:01 -0400

    updated default search

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
-    componentDidMount(){
-        this.onTermSubmit('buildings');
+    componentDidMount() {
+        this.onTermSubmit('toronto');
     }
 
     onTermSubmit = async (term) => {
@@ -17,9 +17,10 @@ class App extends React.Component {
                 q: term
             }
         });
-        this.setState({ 
+        this.setState({
             videos: response.data.items,
-            selectedVideo: response.data.items[0] });
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
@@ -33,11 +34,11 @@ class App extends React.Component {
                 <div className="ui grid">
                     <div className="ui row">
                         <div className="eleven wide column">
-                        <VideoDetail video={this.state.selectedVideo} />
+                            <VideoDetail video={this.state.selectedVideo} />
                         </div>
                         <div className="five wide column">
-                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div> 
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
                     </div>
                 </div>
             </div>
